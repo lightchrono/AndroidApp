bdbe7b34a8c16ee454d84c3c7c452bd74b48ca4d
balaurguy <neagulaurean@gmail.com>
2017-05-30 19:36:47 +0300

    test

--- app/src/main/java/com/example/light/androidapp/Activities/Register.java
+++ app/src/main/java/com/example/light/androidapp/Activities/Register.java
@@ -2,11 +2,17 @@ package com.example.light.androidapp.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.light.androidapp.R;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Register extends AppCompatActivity {
@@ -24,7 +30,29 @@ public class Register extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        //aici scrii
+                        OutputStreamWriter writer;
+                        DataInputStream reader;
+                        try
+                        {
+                            client=new Socket(InetAddress.getByName("5.15.180.8"),25565);
+                            reader=new DataInputStream (client.getInputStream());
+                            //writer=new OutputStreamWriter(client.getOutputStream());
+                            PrintWriter print=new PrintWriter(client.getOutputStream());
+                            print.println("register");print.flush();
+                            print.println("username");print.flush();
+                            print.println("email");print.flush();
+                            print.println("password");print.flush();
+
+                            Log.d("LOG",reader.readLine());
+                            Log.d("SERVER","Waiting");
+                            String str=reader.readLine();
+                            Log.d("SERVER",str);
+                            Log.d("SERVER","Done");
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
 
 
                     }
