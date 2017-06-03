d44269c993767220a05948f83e19cd8e1c87da68
balaurguy <neagulaurean@gmail.com>
2017-06-03 17:08:16 +0300

    test

--- app/src/main/java/com/example/light/androidapp/Activities/Register.java
+++ app/src/main/java/com/example/light/androidapp/Activities/Register.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.light.androidapp.R;
 
@@ -21,6 +23,7 @@ import java.net.Socket;
 public class Register extends AppCompatActivity {
 
     Socket client;
+    Boolean isError=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +37,72 @@ public class Register extends AppCompatActivity {
                     @Override
                     public void run() {
                         OutputStreamWriter writer;
-                        try
-                        {
-                            client=new Socket(InetAddress.getByName("192.168.0.102"),11155);
-                            BufferedReader reader=new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        try {
+                            client = new Socket(InetAddress.getByName("192.168.0.102"), 11155);
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
                             //writer=new OutputStreamWriter(client.getOutputStream());
-                            PrintWriter print=new PrintWriter(client.getOutputStream());
-                            print.println("register"+"\r\n");print.flush();
-                            print.println("username"+"\r\n");print.flush();
-                            print.println("email"+"\r\n");print.flush();
-                            print.println("password"+"\r\n");print.flush();
+                            PrintWriter print = new PrintWriter(client.getOutputStream());
+                            EditText username = (EditText) findViewById(R.id.register_username);
+                            EditText email = (EditText) findViewById(R.id.register_email);
+                            EditText password = (EditText) findViewById(R.id.register_password);
+                            EditText password2 = (EditText) findViewById(R.id.register_password2);
 
-                            Log.d("LOG",reader.readLine());
-                            Log.d("SERVER","Waiting");
-                            String str=reader.readLine();
-                            Log.d("SERVER",str);
-                            Log.d("SERVER",reader.readLine());
-                            Log.d("SERVER","Done");
-                        }
-                        catch (IOException e)
-                        {
+
+                            TextView usernametv = (TextView) findViewById(R.id.register_usernametv);
+                            try {
+                                usernametv.setText("");
+                            }catch (Exception ex){}
+                            TextView emailtv = (TextView) findViewById(R.id.register_emailtv);
+                            try {
+                                emailtv.setText("");
+                            }catch (Exception ex){}
+                            TextView passwordtv = (TextView) findViewById(R.id.register_passwordtv);
+                            try {
+                                passwordtv.setText("");
+                            }catch (Exception ex){}
+                            if (password.getText().toString().equals(password2.getText().toString())) {
+                                print.println("register" + "\r");
+                                print.flush();
+                                print.println(username.getText() + "\r");
+                                print.flush();
+                                print.println(email.getText() + "\r");
+                                print.flush();
+                                print.println(password.getText() + "\r");
+                                print.flush();
+
+
+                                Log.d("SERVER", "emailcheck");
+
+                                String emailCheck=reader.readLine();
+                                String userCheck=reader.readLine();
+                                if (emailCheck.equals( "emailfail")) {
+                                    try {
+                                        Log.d("SERVER", "emailfail");
+                                        emailtv.setText("email already taken");
+                                    } catch (Exception ex) {
+                                    }
+                                } else Log.d("SERVER", "emailgood");
+                                Log.d("SERVER", "userCheck");
+                                if (userCheck.equals("userfail")) {
+                                    try {
+                                        Log.d("SERVER", "userfail");
+                                        usernametv.setText("user already taken");
+                                    } catch (Exception ex) {
+                                    }
+                                } else Log.d("SERVER", "userGood");
+                            } else {
+                                try {
+                                    passwordtv.setText("Password didn't match");
+                                } catch (Exception ex) {
+                                }
+                            }
+                            Log.d("SERVER", "reader");
+                            String registerCheck=reader.readLine();
+                            if(registerCheck.equals("registergood")){
+                                Intent login=new Intent(getBaseContext(),Login.class);
+                                startActivity(login);
+                            }
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
 
